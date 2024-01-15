f71b14599e5e014cbcefb32f6945b35ea1178afc
t0nkisa <antanitonev03@gmail.com>
2024-01-15 17:01:09 +0100

    added an example API call

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://db.ygoprodeck.com/api/v7/randomcard.php"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        JSONObject json = new JSONObject(response.body());
+
+        for (String key : json.keySet()) {
+            System.out.print(key);
+            System.out.print(" ");
+            System.out.println(json.get(key));
+        }
+
     }
 }
